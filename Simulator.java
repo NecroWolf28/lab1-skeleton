4882ac493b8f1871fd433e3d1a3f21f0f964910f
Wei Tsang Ooi <weitsang@yahoo.com>
2021-01-27 14:30:18 +0800

    Add javadoc

--- Simulator.java
+++ Simulator.java
@@ -1,8 +1,26 @@
 import java.util.PriorityQueue;
 
+/**
+ * This class implements a discrete event simulator.
+ * The simulator maintains a priority queue of events,
+ * and run through the events, simulate each one until 
+ * the queue is empty.
+ *
+ * @author Wei Tsang
+ * @version CS2030S AY20/21 Semester 2
+ */
 public class Simulator {
+  /** The event queue. */
   private final PriorityQueue<Event> events;
 
+  /** 
+   * The constructor for a simulator.  It takes in
+   * a simulation as an argument, and calls the 
+   * getInitialEvents method of that simulation to
+   * initialize the event queue.
+   * 
+   * @param simulation The simulation to simulate.
+   */
   public Simulator(Simulation simulation) {
     events = new PriorityQueue<Event>();
     for (Event e : simulation.getInitialEvents()) {
@@ -10,6 +28,14 @@ public class Simulator {
     }
   }
 
+  /**
+   * Run the simulation until no more events is in
+   * the queue.  For each event in the queue (in
+   * increasing order of time), print out its string 
+   * representation, then simulate it.  If the 
+   * simulation returns one or more events, add them 
+   * to the queue, and repeat.
+   */
   public void run() {
     Event event = this.events.poll();
     while (event != null) {
